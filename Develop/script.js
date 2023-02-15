1dd2d4c10c13dde5a3f2c32a3ad03bad6ec42de2
Kevin Rhode <kevinrhode@hotmail.com>
2023-02-15 18:02:39 -0500

    add: added comments to code for later ref, small padding add in style.css

--- Develop/script.js
+++ Develop/script.js
@@ -14,7 +14,7 @@ var genPassLength = document.getElementById("pass-length");
 // Assignment code here
 function generatePassword(){
 
-  //Basic password selection
+  //Basic password selection - local ref to recreate so no string options are still present
 var charset="ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmopqrstuvwxyz";
 
 
@@ -25,20 +25,26 @@ var charset="ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmopqrstuvwxyz";
 //include space - if changed
   chkIncludeSpace = document.getElementById("space").checked;
 
+  //if true include special char in the available char's of the random password 
   if (chkIncludeSpecialChar) {
     charset += "!\"#$%&'()*+,-./:;<=>?@[\\]^_`{|}~";
   }
-
+  //if true include numbers in the available char's of the random password 
   if (chkIncludeNumber) {
     charset += "0123456789";
   }
-
+  //if true include space in the available char's of the random password 
   if (chkIncludeSpace) {
     charset += " ";
   }
+  //value of the password to be returned
   var retValue = "";
+
   for (let index = 0 , fit = charset.length; index < genPassLength.value; index++) {
-        retValue += charset.charAt(Math.floor(Math.random() * fit))
+      //math.random - can be equal or greater then 0, and less then 1.
+      //we use floor to get whole int
+      //random * fit to give value between start and end of string. as 0 is starting point
+      retValue += charset.charAt(Math.floor(Math.random() * fit))
   }
 
   // we are returning someting to the var password
@@ -49,10 +55,13 @@ var charset="ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmopqrstuvwxyz";
 
 
 // Write password to the #password input
-function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
 
+function writePassword() {
+  //generate the password
+  var password = generatePassword();
+  //find and select the element with id password
+  var passwordText = document.querySelector("#password");
+  //set the .value of the element to password
   passwordText.value = password;
 
 }
